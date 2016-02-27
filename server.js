2b2df4f4bc0d40910852843091eb0148b62a6401
allimac <camillacolser@hotmail.com>
2016-02-27 12:35:30 +0000

    responding to / endpoint

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var http = require('http');
+var fs = require('fs');
 
-var message = "I\'m very happy this morning!";
+// var message = "I\'m very happy this morning!";
 
 function handler(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write(message);
-  res.end();
+  var endpoint = req.url;
+  var method = req.method;
+
+  if (endpoint === '/') {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      res.end(file);
+    });
+  }
+  console.log(endpoint);
+  console.log(method);
 }
 
 var server = http.createServer(handler);
