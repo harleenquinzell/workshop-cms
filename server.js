aeda0f06df4a3ff04dfa270a27a8cd1802821f17
allimac <camillacolser@hotmail.com>
2016-02-27 15:30:44 +0000

    separated handler into new js file

--- server.js
+++ server.js
@@ -1,51 +1,5 @@
 var http = require('http');
-var fs = require('fs');
-var querystring = require('querystring');
-
-function handler(req, res) {
-  var endpoint = req.url;
-  var method = req.method;
-  var allTheData = '';
-
-  if (endpoint === '/') {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile(__dirname + '/public/index.html', function(error, file) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      res.end(file);
-    });
-
-  } else if (endpoint === '/create-post') {
-
-    req.on('data', function (chunkOfData) {
-      allTheData += chunkOfData;
-    });
-
-    req.on('end', function (){
-      res.writeHead(302, {"Location": '/'});
-      var convertedData = querystring.parse(allTheData);
-      console.log(convertedData);
-
-      res.end();
-    });
-
-  } else {
-
-    var fileExtension = endpoint.split('.')[1];
-
-    res.writeHead(200, {"Content-Type": "text/" + fileExtension });
-    fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      res.end(file);
-
-    });
-  }
-}
+var handler = require('./src/handler');
 
 var server = http.createServer(handler);
 
---
+++ src/handler.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var querystring = require('querystring');
+
+function handler(req, res) {
+  var endpoint = req.url;
+  var method = req.method;
+  var allTheData = '';
+
+  if (endpoint === '/') {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      res.end(file);
+    });
+
+  } else if (endpoint === '/create-post') {
+
+    req.on('data', function (chunkOfData) {
+      allTheData += chunkOfData;
+    });
+
+    req.on('end', function (){
+      res.writeHead(302, {"Location": '/'});
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+
+      res.end();
+    });
+
+  } else {
+
+    var fileExtension = endpoint.split('.')[1];
+
+    res.writeHead(200, {"Content-Type": "text/" + fileExtension });
+    fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      res.end(file);
+
+    });
+  }
+}
+
+module.exports = handler;
