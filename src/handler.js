b5945e454a57ad00dbcb431781012528aec94f61
allimac <camillacolser@hotmail.com>
2016-02-27 16:58:53 +0000

    writing json blog posts ... wip

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var querystring = require('querystring');
 
 function handler(req, res) {
   var endpoint = req.url;
@@ -12,17 +13,40 @@ function handler(req, res) {
       }
       res.end(file);
     });
-  } else if (endpoint === '/create/post') {
-    var allData = '';
 
-    req.on('data', function(data) {
-      allData += data;
+  } else if (endpoint === '/create/post') {
+    var jsonFile = './src/posts.json';
+    var blogPosts;
+    var newPost;
+    var allTheData = '';
+    var timestamp = Date.now();
+
+    req.on('data', function(incomingData) {
+      allTheData += incomingData;
     });
 
     req.on('end', function() {
-      res.writeHead(302, { "Location" : "/" });
-      console.log(allData);
-      res.end();
+      console.log('got all data!', allTheData);
+      newPost = querystring.parse(allTheData);
+
+      fs.readFile(jsonFile, function(error, file) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        blogPosts = JSON.parse(file);
+        blogPosts[timestamp] = newPost;
+
+        fs.writeFile(jsonFile, JSON.stringify(blogPosts), function(error) {
+          console.log(blogPosts);
+          if (error) {
+            console.log(error);
+            return;
+          }
+          res.writeHead(302, { "Location" : "/" });
+          res.end();
+        });
+      });
     });
 
   } else {
